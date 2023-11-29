493c2277106860fb78f43787ab0ff72730016bb2
ds-usova <ds.usova@mail.ru>
2023-11-29 23:04:47 +0100

    Create ProductCard.vue component

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,24 +1,18 @@
 <template>
   <b-row id="main-page" class="pt-0">
     <b-col v-if="isLoading" class="col-3" v-for="n in 12">
-      <b-card class="mb-2">
-        <b-spinner type="grow" label="Spinning"></b-spinner>
-        <b-card-body>
-          <b-card-title></b-card-title>
-          <star-rating :value="5" :max="5" :reviews-count="0" />
-          <b-card-subtitle class="mt-0">$0</b-card-subtitle>
-        </b-card-body>
-      </b-card>
+      <product-card>
+        <b-spinner type="grow"></b-spinner>
+      </product-card>
     </b-col>
     <b-col v-if="!isLoading" class="col-3" v-for="(product, index) in products" :key="index">
-      <b-card class="mb-2">
+      <product-card :title="product.title"
+                    :rating="product.rating.rate"
+                    :ratingMax="5"
+                    :reviews-count="product.rating.count"
+                    :price="product.price">
         <img class="card-img-top mb-3" :src="product.image" :alt="product.title"/>
-        <b-card-body>
-          <b-card-title>{{ product.title }}</b-card-title>
-          <star-rating :value="product.rating.rate" :max="5" :reviews-count="product.rating.count" />
-          <b-card-subtitle class="mt-0">${{ product.price }}</b-card-subtitle>
-        </b-card-body>
-      </b-card>
+      </product-card>
     </b-col>
   </b-row>
 </template>
@@ -27,7 +21,8 @@
 import axios from "axios";
 import {onMounted, ref} from "vue";
 import {Product} from "../model/Product";
-import StarRating from "./StarRating.vue";
+
+import ProductCard from "./ProductCard.vue";
 
 const products = ref<Array<Product>>([])
 const isLoading = ref(true)
@@ -42,22 +37,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.card {
-  border-color: transparent
-}
-
-.card-title {
-  color: rgb(120, 122, 120);
-  text-align: start;
-  font-size: 14px;
-}
-
-.card-subtitle {
-  color: rgb(120, 122, 120);
-  font-size: 12px;
-  text-align: start;
-}
-
 .card-img-top {
   width: 100%;
   height: 200px;
--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,8 +1,10 @@
 <template>
-  <font-awesome-icon class="fa-xs" v-for="n in fullStarsCount" icon="star" />
-  <font-awesome-icon class="fa-xs" v-if="hasHalfStar" :icon="['fas', 'star-half']" />
-  <font-awesome-icon class="fa-xs" v-for="n in emptyStarCount" :icon="[ 'far', 'star' ]" />
-  <span class="align-text-top">{{props.reviewsCount}} reviews</span>
+  <div>
+    <font-awesome-icon class="fa-xs" v-for="n in fullStarsCount" icon="star" />
+    <font-awesome-icon class="fa-xs" v-if="hasHalfStar" :icon="['fas', 'star-half']" />
+    <font-awesome-icon class="fa-xs" v-for="n in emptyStarCount" :icon="[ 'far', 'star' ]" />
+    <span class="align-text-top">{{props.reviewsCount}} reviews</span>
+  </div>
 </template>
 
 <script setup>
@@ -18,5 +20,9 @@ span {
   font-size: 13px;
   margin-left: 5px;
   color: rgb(120, 122, 120);
+}
+
+div {
+  text-align: left;
 }
 </style>

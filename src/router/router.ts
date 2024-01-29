f49a1af834b2e8606552fb8fe84a5a9fb141e2dd
ds-usova <ds.usova@mail.ru>
2024-01-29 23:58:24 +0100

    Use dynamic imports

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,22 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router";
 import MainPage from "../pages/MainPage.vue";
-import ProductPage from "../pages/ProductPage.vue";
-import CheckoutPage from "../pages/CheckoutPage.vue";
-import OrderPlacedPage from "../pages/OrderPlacedPage.vue";
-import NewProductPage from "../pages/NewProductPage.vue";
-import LoginPage from "../pages/LoginPage.vue";
-import UserProfilePage from "../pages/UserProfilePage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
 const routes = [
     {path: "/", name: "main", component: MainPage},
-    {path: "/product/:id", name: "product", component: ProductPage},
-    {path: "/checkout", name: "checkout", component: CheckoutPage},
-    {path: "/success", name: "success", component: OrderPlacedPage},
-    {path: "/new-product", name: "new-product", component: NewProductPage},
-    {path: "/login", name: "login", component: LoginPage},
-    {path: "/profile", name: "profile", component: UserProfilePage},
-    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage},
+    {path: "/product/:id", name: "product", component: () => import('../pages/ProductPage.vue')},
+    {path: "/checkout", name: "checkout", component: () => import('../pages/CheckoutPage.vue')},
+    {path: "/success", name: "success", component: () => import('../pages/OrderPlacedPage.vue')},
+    {path: "/new-product", name: "new-product", component: () => import('../pages/NewProductPage.vue')},
+    {path: "/login", name: "login", component: () => import('../pages/LoginPage.vue')},
+    {path: "/profile", name: "profile", component: () => import('../pages/UserProfilePage.vue')},
+    {path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage},
 ];
 
 const router = createRouter({
